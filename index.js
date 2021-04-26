a1a99b5bde9d6709c6bc9ceacf056202be84230b
Amro dandashli <amrdandashli@gmail.com>
2021-04-26 09:19:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,16 +15,21 @@ function submitData("Name","email") {
     body: JSON.stringify(formData)
   };
 
-  fetch("http://localhost:3000/users", configObj)
+returns  fetch("http://localhost:3000/users", configObj)
     .then(function(response) {
       return response.json();
     })
     .then(function(object) {
+      let h2 = document.createElement('h2');
+      h2.innerHTML= object.id ;
+      document.body.appendChild(h2);
       console.log(object);
     });
     .catch(function(error) {
    alert("Bad things! Ragnarők!");
+   let h3 = document.createElement('h3');
+   h3.innerHTML = error.message;
+   document.body.appendChild(h3);
    console.log(error.message);
  });
- return fetch()
 }
